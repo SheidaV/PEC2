ec7e00f8cd77e2da7ec08fea4a3aacf94f4c37aa
Sheida Alfaro Taco <alfarosh@uoc.edu>
2023-05-21 23:17:58 +0200

    fixes PEC1

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,4 @@
-import _ from "lodash";
-
-console.log(_.concat([10,15,20],[40]));
+console.log([10, 15, 20].concat([40]));
 
 let map = L.map('map').setView([41.3624, 2.1065], 12);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
